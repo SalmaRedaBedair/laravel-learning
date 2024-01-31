6b22e55a2ece5800834597499a91f149cecc9fc5
SalmaRedaBedair <salmaredab173@gmail.com>
2024-01-31 22:42:35 +0200

    practice

--- practice/app/Http/Controllers/testController.php
+++ practice/app/Http/Controllers/testController.php
@@ -8,7 +8,6 @@ class testController extends Controller
 {
     public function sent(Request $request)
     {
-        return $request->all();
         if($request->ismethod('post'))
         {
             return $request->all();
